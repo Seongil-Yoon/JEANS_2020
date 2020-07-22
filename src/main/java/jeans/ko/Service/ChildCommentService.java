14e0bd04596e0796c356ea814fe754eed9a1fdba
ogt <ogt6590@gmail.com>
2020-07-22 09:29:17 +0900

    대댓글 값넣기 까지 성공

--- src/main/java/jeans/ko/Service/ChildCommentService.java
+++ src/main/java/jeans/ko/Service/ChildCommentService.java
@@ -4,10 +4,11 @@ import jeans.ko.Dao.IChildCommentDao;
 import jeans.ko.Dao.ICommentDao;
 import jeans.ko.Dto.ChildCommentDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service
 public class ChildCommentService implements IChildCommentService {
 
     @Autowired
@@ -15,13 +16,14 @@ public class ChildCommentService implements IChildCommentService {
 
     @Override
     public int insert(ChildCommentDto childCommentDto) {
-        System.out.println(childCommentDto+"  asdasd");
+
         return childCommentDao.insert(childCommentDto);
+
     }
 
     @Override
     public ChildCommentDto ChildComment(int child_comment_num) {
-        return childCommentDao.childComment(child_comment_num);
+            return childCommentDao.childComment(child_comment_num);
     }
 
     @Override
