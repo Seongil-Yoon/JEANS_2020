5e57556a33e45ae44dd52dabc5107bb07e708865
moomin97 <dbs3346@gmail.com>
2020-08-11 16:53:32 +0900

    파일폰드실험실_글수정완료

--- build/resources/main/static/js/lookModify/lookModify.js
+++ build/resources/main/static/js/lookModify/lookModify.js
@@ -39,43 +39,74 @@ function modifiy() {
     formData.append("BoardDto", JSON.stringify(BoardDto));
 
     if (empty == '') {
-        let data = $('[name=writeForm]').serialize();
+        if (fileBuffer == undefined) {
+            console.log("fileBuffer == undefined");
+            let data = $('[name=writeForm]').serialize();
+            //글수정하기
+            $.ajax({
+                url: "/looks",
+                type: "PUT", //데이터 전달방식
+                data: formData, //전송객체
+                dataType: false,
+                processData: false,
+                // contentType: 'multipart/form-data',
+                contentType: false,
+                success: function (result, textStatus, jqxHR) {
+                    if (jqxHR.status == 201) {
+                        swal('', '게시글 수정을 하였습니다', 'success');
+                        //수정 성공하면 내가수정한 게시글화면으로 이동
+                        setTimeout(function () { location.href = "/look?look_num=" + result.look_num; }, 2000);
+                    }
+                },
+                error: function (error) {
+                    //서버오류 500  찾는 자료없음 404  권한없음  401
+                    if (error.status == 404) {
+                        swal('수정 자료가 없습니다', '', 'error');
+                    } else if (error.status == 401) {
+                        swal('접근 권한이 없습니다', '', 'error');
+                    } else if (error.status == 500) {
+                        swal('서버 오류 관리자에게 문의 하세요', '', 'error');
+                    }
+                }
+            })
+        } else {
+            let data = $('[name=writeForm]').serialize();
+            function appendFile() {
+                for (i = 0; i < fileBuffer.length; i++) {
+                    formData.append("files", fileBuffer[i]);
+                }
 
-        function appendFile() {
-            for (i = 0; i < fileBuffer.length; i++) {
-                formData.append("files", fileBuffer[i]);
             }
+            appendFile();
 
+            //글수정하기
+            $.ajax({
+                url: "/looks",
+                type: "PUT", //데이터 전달방식
+                data: formData, //전송객체
+                dataType: false,
+                processData: false,
+                // contentType: 'multipart/form-data',
+                contentType: false,
+                success: function (result, textStatus, jqxHR) {
+                    if (jqxHR.status == 201) {
+                        swal('', '게시글 수정을 하였습니다', 'success');
+                        //수정 성공하면 내가수정한 게시글화면으로 이동
+                        setTimeout(function () { location.href = "/look?look_num=" + result.look_num; }, 2000);
+                    }
+                },
+                error: function (error) {
+                    //서버오류 500  찾는 자료없음 404  권한없음  401
+                    if (error.status == 404) {
+                        swal('수정 자료가 없습니다', '', 'error');
+                    } else if (error.status == 401) {
+                        swal('접근 권한이 없습니다', '', 'error');
+                    } else if (error.status == 500) {
+                        swal('서버 오류 관리자에게 문의 하세요', '', 'error');
+                    }
+                }
+            })
         }
-        appendFile();
-
-        //글수정하기
-        $.ajax({
-            url: "/looks",
-            type: "PUT", //데이터 전달방식
-            data: formData, //전송객체
-            dataType: false,
-            processData: false,
-            // contentType: 'multipart/form-data',
-            contentType: false,
-            success: function (result, textStatus, jqxHR) {
-                if (jqxHR.status == 201) {
-                    swal('', '게시글 수정을 하였습니다', 'success');
-                    //수정 성공하면 내가수정한 게시글화면으로 이동
-                    setTimeout(function () { location.href = "/look?look_num=" + result.look_num; }, 2000);
-                }
-            },
-            error: function (error) {
-                //서버오류 500  찾는 자료없음 404  권한없음  401
-                if (error.status == 404) {
-                    swal('수정 자료가 없습니다', '', 'error');
-                } else if (error.status == 401) {
-                    swal('접근 권한이 없습니다', '', 'error');
-                } else if (error.status == 500) {
-                    swal('서버 오류 관리자에게 문의 하세요', '', 'error');
-                }
-            }
-        })
     } else {
         //입력안한 부분있으면 다시작성하게함
         swal('', empty + ' 을 다시 입력하세요', '');
